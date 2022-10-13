1a2a9b62a5487aacf88e0914ec6ff37309fa697a
EliasH96 <elias.banco15@gmail.com>
2022-10-13 15:40:15 -0300

    Try homework

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,9 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y) return x
+  else if (y > x) return y 
+  else return x || y
 }
 
 function mayoriaDeEdad(edad) {
@@ -25,6 +28,9 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+if (status === 1) return "Online"
+else if (status ===2) return "Away"
+else return "Offline"
 }
 
 function saludo(idioma) {
