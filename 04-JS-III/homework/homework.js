0de6a30273058eb2d83506f959a1249961f9a44f
EliasH96 <elias.banco15@gmail.com>
2022-10-15 20:02:47 -0300

    DiaSemana

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -120,7 +120,13 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+var Elements = 0
+for ( let i = 0; i < arreglo.length; i++) {
+if (arreglo[i] >18) {
+  Elements = (Elements + 1)
+}
+} 
+return Elements
 }
 
 
