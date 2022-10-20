7f8585db5741eba077b45e8bfaf8811db2720deb
EliasH96 <elias.banco15@gmail.com>
2022-10-20 19:49:13 -0300

    filter

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -18,6 +18,7 @@ function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -25,12 +26,19 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var suma = numeros.reduce(function(acc, curr) {
+    return acc + curr;
+  }, 0);
+  cb(suma)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+for (var i =0; i < array.length; i++) {
+  cb(array[i])
+}
 }
 
 function map(array, cb) {
@@ -38,6 +46,10 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nuevoArray = array.map(function(el) {
+    return cb (el)
+  })
+     return nuevoArray
 }
 
 function filter(array) {
