b9964d6b541bd637acd40bb4f40cbbed58e08c4b
EliasH96 <elias.banco15@gmail.com>
2022-11-03 20:17:40 -0300

    try home

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,13 +10,12 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
-    var matriz = Object.entries(objeto);
-    for(var i = 0; i < matriz.length; i++) {
-      for(var z = 0; z < matriz[i].length; z++) {
-        return (matriz[i][z] + " ");
-      }
-    }
-}
+    var matriz = [];
+     for(let key in objeto) {
+      matriz.push([key, objeto[key]])
+     }
+    return matriz
+    } 
 
 
 function numberOfCharacters(string) {
@@ -24,7 +23,15 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
-  
+  let objeto = {}
+  for(let i = 0; i<string.length; i++) {
+    if(Object.keys(objeto).includes(string[i])) {
+      objeto[string[i]] = objeto[string[i]] + 1
+      continue 
+    }
+    objeto[string[i]] = 1
+  }
+  return objeto
 }
 
 
@@ -33,6 +40,16 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  let minusculas = ""
+  let mayusculas = ""
+  for(let i = 0; i<s.length; i++) {
+    if(s[i] === s[i].toUpperCase()) {
+      mayusculas = mayusculas + s[i]
+    }
+    else {
+      minusculas = minusculas + s[i]
+    }
+  } return mayusculas + minusculas
 }
 
 
